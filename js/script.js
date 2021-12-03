78d4dfe0eabba9653d702395eb0049b8de9dd9b7
MemeWasTaken <memelazzari02@gmail.com>
2021-12-03 14:41:49 +0100

    add 5 Rdm numbers to Dom

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // 3) Tramite 5 prompt() l'utente deve inserire i numeri visti;
 // 4) Software visualizza quanti e quali numeri sono stati indovinati.
 
+// 1) //
+const arrayNumber = [];
+let listNumberDom = document.getElementById('list-number-dom');
+
+for (let i = 0; i < 5; i++) {
+    let number =  Math.floor(Math.random() * (100 - 1 + 1) ) + 1;
+    arrayNumber.push(number);
+    let li = document.createElement('li');
+    li.append(number);
+    listNumberDom.append(li);
+}
+console.log(arrayNumber);
