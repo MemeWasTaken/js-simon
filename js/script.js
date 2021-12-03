0567a87125bc4c2502ee0e18c50126cde4699a77
MemeWasTaken <memelazzari02@gmail.com>
2021-12-03 14:27:31 +0100

    problem in words

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1) Creare 5 numeri casuali e visualizzarli in pagina;
+// 2) Visualizzarli per 30 secondi dopodiché spariscono;
+// 3) Tramite 5 prompt() l'utente deve inserire i numeri visti;
+// 4) Software visualizza quanti e quali numeri sono stati indovinati.
+
